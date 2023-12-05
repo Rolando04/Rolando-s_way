43425b0ca43f7141ee77a57f79cf26010ec084e5
ChronicAtom533 <89808848+Rolando04@users.noreply.github.com>
2023-12-05 16:17:53 -0600

    Add files via upload

---
+++ room.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "room.h"
+
+roomInfo* createRooms(FILE fileIn){
+    
+}
---
+++ room.h
@@ -0,0 +1,11 @@
+#ifndef ROOM_H
+#define ROOM_H
+
+typedef struct roomInfo_struct{
+char roomId[5];
+char tvAsset[7];
+char tvSerial[13];
+}roomInfo;
+
+
+#endif
